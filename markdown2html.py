c0e71045f249eb6c721e330f46250c2ff0fd8e38
Dannyelgeek <daot1993@gmail.com>
2023-08-11 07:57:39 -0500

    modify task 0 excercise

--- markdown2html.py
+++ markdown2html.py
@@ -11,12 +11,12 @@ def start_script():
     arg = len(sys.argv)
 
     if arg < 3:
-        sys.stderr.write('Usage: ./markdown2html.py README.md README.html')
+        sys.stderr.write('Usage: ./markdown2html.py README.md README.html ')
         exit(1)
     try:
         open('{}'.format(sys.argv[1]))
     except:
-        sys.stderr.write('Missing {}'.format(sys.argv[1]))
+        sys.stderr.write('Missing  {}'.format(sys.argv[1]))
         exit(1)
 
 if __name__=="__main__":
